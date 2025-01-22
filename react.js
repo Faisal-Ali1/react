c887036e9816096258a79f6a7da0b835b057b800
Faisal Ali <fali77616@gmail.com>
2025-01-22 19:58:11 +0530

    style the elements

--- react.js
+++ react.js
@@ -1,8 +1,8 @@
-let element = React.createElement('h1' , {} , "Hllo World");
+let element = React.createElement('h1' , {id:"header1", className:"head" , style : { color : "white" , backgroundColor : "black" , paddingLeft: "1rem"}} , "Hllo World");
 
 let main = ReactDOM.createRoot(document.querySelector("#contaner"));
 main.render(element);
 
-let element1 = React.createElement('h2' , {} , 'Bye World');
+let element1 = React.createElement('h2' , {id:"header2" , className:"head" , style:{backgroundColor: "brown" , color:"white" , paddingLeft:"1rem"}} , 'Bye World');
 let element2 = React.createElement('div' , {} , [element , element1]);
 main.render(element2)
