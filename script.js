be145e43444a9b71d78771ca26ebb23751fc911c
Faisal Ali <fali77616@gmail.com>
2025-01-22 10:35:48 +0530

    my third comit

--- script.js
+++ script.js
@@ -1,60 +1,40 @@
-let contaner = document.querySelector("#contaner");
 
-let React = {
-    createElement: (tag, content, styles) => {
+// for creating element
+const React = {
+    createElement : function(tag , property , text){
+        let element = document.createElement(tag);
 
-        let head = document.createElement(tag);
-
-        // this section is for creating li and appending it in ul
-        if (Array.isArray(content) && tag === 'ul') {
-            for (let item of content) {
+        if(Array.isArray(text)){
+            for(let value of text){
                 let list = document.createElement("li");
-                list.innerText = item;
-                head.append(list);
-            }
-
-        }
-        // this section is for creating tags and append it in div 
-        else if (typeof content === 'object' && tag === 'div') {
-
-            for (let [key , value] in Object.entries(content)) {
-                if (key === 'ul') {
-                    let box = document.createElement(key);
-                    for (let values of value) {
-                        let list = document.createElement("li")
-                        list.innerText = values;
-                        box.append(list);
-                    }
-                    head.append(box);
-
-                }
-                else {
-                    let element = document.createElement(key);
-                    element.innerText = content[key];
-                    head.append(element);
-                }
-
+                list.innerText = value;
+                element.append(list);
             }
         }
-        else {
-            head.innerText = content;
+        else{
+            element.innerText = text;
+        }
+        
+        for(let key in property){
+            element.style[key] = property[key];
         }
 
-        // Styling area
-        for (let key in styles) {
-            head.style[key] = styles[key]
-        }
+        return element;
+    },
+}
 
-
-        contaner.append(head);
-
+// for manuplating DOM / rendering on web
+const ReactDom = { 
+    render : function(element , root){
+        root.append(element);
     }
 }
 
+let header1 = React.createElement('h1' , {'backgroundColor' : 'black' , 'color' : 'white'} , "Hllo World");
+let header2 = React.createElement('h2' , {'backgroundColor' : 'brown' , 'color' : 'white'} , 'How are you all?');
+let header3 = React.createElement('ul' , {} , ['HTML' , 'CSS' , 'JS' , 'TS']);
 
-React.createElement("h1", "Hllo World", { 'backgroundColor': 'black', 'color': 'white' });
-React.createElement("p", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui in enim maxime quae fuga doloremque neque dolorem eum officia! Error aspernatur expedita suscipit debitis illum iure accusantium accusamus alias neque!", { 'backgroundColor': 'antiquewhite', 'color': 'black', 'padding': '1rem', 'margin': '1rem', 'textAlign': 'center' });
-React.createElement('ul', ['HTML', 'CSS', "JS", 'TS', 'REACT', 'NODE JS'], { 'border': '1px solid black', 'width': '10rem' });
-React.createElement('div', { 'h2': "Quantum Computers", 'p': "Hllo how are you" }, { 'backgroundColor': 'black', 'color': 'white', 'padding': '0.5rem 1rem' });
+ReactDom.render(header1 , document.querySelector("#contaner"));
+ReactDom.render(header2 , document.querySelector("#contaner"));
+ReactDom.render(header3 , document.querySelector("#contaner"));
 
-React.createElement('div', { 'ul': ['HTML', 'CSS', 'JS', 'TS'] }, {});
