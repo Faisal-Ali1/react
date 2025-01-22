3be84edbd06c27ed59a1c2e5015e0bbc93898b6f
Faisal Ali <fali77616@gmail.com>
2025-01-22 20:04:27 +0530

    add new heading

--- react.js
+++ react.js
@@ -1,8 +1,11 @@
 let element = React.createElement('h1' , {id:"header1", className:"head" , style : { color : "white" , backgroundColor : "black" , paddingLeft: "1rem"}} , "Hllo World");
 
+let element1 = React.createElement('h2' , {id:"header2" , className:"head" , style:{backgroundColor: "brown" , color:"white" , paddingLeft:"1rem"}} , 'Bye World');
+
+let element3 = React.createElement('h3' , {} , "Where are you ?");
 let main = ReactDOM.createRoot(document.querySelector("#contaner"));
 main.render(element);
 
-let element1 = React.createElement('h2' , {id:"header2" , className:"head" , style:{backgroundColor: "brown" , color:"white" , paddingLeft:"1rem"}} , 'Bye World');
-let element2 = React.createElement('div' , {} , [element , element1]);
+
+let element2 = React.createElement('div' , {id:"header3" , className:"head"} , [element , element1 , element3]);
 main.render(element2)
