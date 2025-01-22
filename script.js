b7e161c993a07bdcf5465123a1e3b81219e700b7
Faisal Ali <fali77616@gmail.com>
2025-01-22 11:30:18 +0530

    My fourth commit

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 
-// for creating element
-const React = {
-    createElement : function(tag , property , text){
-        let element = document.createElement(tag);
+// // for creating element
+// const React = {
+//     createElement : function(tag , property , text){
+//         let element = document.createElement(tag);
 
-        if(Array.isArray(text)){
-            for(let value of text){
-                let list = document.createElement("li");
-                list.innerText = value;
-                element.append(list);
-            }
-        }
-        else{
-            element.innerText = text;
-        }
+//         if(Array.isArray(text)){
+//             for(let value of text){
+//                 element.append(value);
+//             }
+//         }
+//         else{
+//             element.innerText = text;
+//         }
         
-        for(let key in property){
-            element.style[key] = property[key];
-        }
+//         for(let key in property){
+//             element.style[key] = property[key];
+//         }
 
-        return element;
-    },
-}
+//         return element;
+//     },
+// }
 
-// for manuplating DOM / rendering on web
-const ReactDom = { 
-    render : function(element , root){
-        root.append(element);
-    }
-}
+// // for manuplating DOM / rendering on web
+// const ReactDom = { 
+//     render : function(element , root){
+//         root.append(element);
+//     }
+// }
 
-let header1 = React.createElement('h1' , {'backgroundColor' : 'black' , 'color' : 'white'} , "Hllo World");
-let header2 = React.createElement('h2' , {'backgroundColor' : 'brown' , 'color' : 'white'} , 'How are you all?');
-let header3 = React.createElement('ul' , {} , ['HTML' , 'CSS' , 'JS' , 'TS']);
+// let header1 = React.createElement('h1' , {'backgroundColor' : 'black' , 'color' : 'white'} , "Hllo World");
+// let header2 = React.createElement('h2' , {'backgroundColor' : 'brown' , 'color' : 'white'} , 'How are you all?');
+// let li1 = React.createElement('li' , {} , 'HTML');
+// let li2 = React.createElement('li' , {} , 'CSS');
+// let li3 = React.createElement('li' , {} , 'JS');
+// let li4 = React.createElement('li' , {} , 'TS');
+// let ul = React.createElement('ul' , {} , [li1 , li2 , li3 , li4]);
 
-ReactDom.render(header1 , document.querySelector("#contaner"));
-ReactDom.render(header2 , document.querySelector("#contaner"));
-ReactDom.render(header3 , document.querySelector("#contaner"));
+// ReactDom.render(header1 , document.querySelector("#contaner"));
+// ReactDom.render(header2 , document.querySelector("#contaner"));
+// ReactDom.render(ul , document.querySelector("#contaner"));
 
